338d4465549f70cfa6406dad3680297bb248105d
Isaac <idemello@hawaii.edu>
2018-11-06 21:41:10 -1000

    code now integrates with front end

--- server.py
+++ server.py
@@ -12,56 +12,43 @@ from tensorflow.python.keras.utils import np_utils
 import csv
 import json
 
-
-def EncodeDecode(img):
-    #Get the type of encoding the bytes are saved in from the chardet.detect call
-    #chardet returns a dictonary
-    #ex: {'encoding': 'ascii', 'confidence': 1.0, 'language': ''}
-    encode_type = list(chardet.detect(img).values())
-    
-    if encode_type[0] == None:
-        pass
-    #once encoding is decode back into a string
-    else:
-        img = img.decode(encode_type[0])
-        img = img.encode('utf-8')
-    #encode once again in base64 to ensure all information is received as a base64 byte str
-    
-    img = base64.b64encode(img)
-
-    return img
-
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         sumplant = []
         #obtain the length of the message being sent
         content_length = int(self.headers['Content-Length'])
-        
+        print('content')
+        print(content_length)
         #parse the information
         body = self.rfile.read(content_length)
-        
+        payload = json.loads(body)
+        print('json')
+        print(body)
+
+        image = list(payload.values())
+        body = image[0]
+        print(type(body))
+        print('body =========')
+       # print(body[17:-2])
         #confirm successful request
         self.send_response(200)
         self.end_headers()
         
         #determine what type of encoding the file is in by calling
-        #EncodeDecode to obtain ensure encoding is base64
-        body = EncodeDecode(body)
-
+        print(type(body))
         #decode the file and download into pwd
         decode = base64.b64decode(body)
-        output_file = open("new_aloe.jpg", "wb")
+        output_file = open("new_img.jpg", "wb")
         output_file.write(decode)
         output_file.close()
-
 
         NETp = '../MeaKanu'
         Best_weightsp = 'Best-weights-PLANT'
         #'NewOrchBestPLANT'
         data_listp = []
-        print(type(cv2.imread('./new_aloe.jpg')))
-        input_imgp =cv2.imread('./new_aloe.jpg')
+        print(type(cv2.imread('./new_img.jpg')))
+        input_imgp =cv2.imread('./new_img.jpg')
         input_imgp_resize=cv2.resize(input_imgp,(224,224))
         data_listp.append(input_imgp_resize)
         img_datap = np.array(data_listp)
@@ -117,5 +104,5 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         #self.end_headers()
 
 
-httpd = HTTPServer(('localhost', 8080), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('', 8081), SimpleHTTPRequestHandler)
 httpd.serve_forever()
