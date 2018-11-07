3353c51896ad0c5e69e6475e4c5b3d9c5281d4bc
Isaac <idemello@hawaii.edu>
2018-11-06 16:53:40 -1000

    fine grain identification added to server

--- server.py
+++ server.py
@@ -5,6 +5,12 @@ import os
 import base64
 import chardet
 import socket
+from tensorflow.python.keras.models import model_from_json
+import numpy as np
+import os,cv2
+from tensorflow.python.keras.utils import np_utils
+import csv
+
 
 def EncodeDecode(img):
     #Get the type of encoding the bytes are saved in from the chardet.detect call
@@ -32,6 +38,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(b'Hello, world!')
 
     def do_POST(self):
+        sumplant = []
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         self.send_response(200)
@@ -44,6 +51,70 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         output_file = open("new_aloe.jpg", "wb")
         output_file.write(decode)
         output_file.close()
+
+        NETp = '../MeaKanu'
+        Best_weightsp = 'Best-weights-PLANT'
+        #'NewOrchBestPLANT'
+        data_listp = []
+        print(type(cv2.imread('./new_aloe.jpg')))
+        input_imgp =cv2.imread('./new_aloe.jpg')
+        input_imgp_resize=cv2.resize(input_imgp,(224,224))
+        data_listp.append(input_imgp_resize)
+        img_datap = np.array(data_listp)
+        img_datap = img_datap.astype('float32')
+        img_datap = img_datap/255
+        #print (img_datap.shape)
+        json_filep = open('./' + 'model.json', 'r')
+        loaded_model_json = json_filep.read()
+        json_filep.close()
+        modelp = model_from_json(loaded_model_json)
+        # load weights into new model
+        modelp.load_weights('./' + Best_weightsp +'.hdf5')
+        # evaluate loaded model on test data
+        modelp.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        ##### IMPORTANT: OUTPUT ALL ##########
+        outputp = modelp.predict(img_datap)
+        for i in range(0,100): 
+            sumplant.append(sum(outputp[:,i]))
+        else:
+            for i in range(0,100): 
+                sumplant.append(0)
+        ###########################################################################
+                numbTILES = 1
+                FINAL =[]
+                ClassPlantPred =[]
+                PlantPredClass=[]
+                TilesPlant=[]
+                Z=[]
+
+        for j in range(1,6):
+            ClassPlantPred.append(sorted(set(sumplant))[-j])
+            PlantPredClass.append(sumplant.index(ClassPlantPred[j-1]))
+            Z = [x for _,x in sorted(zip(ClassPlantPred,PlantPredClass), reverse=True)]
+
+        print(Z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         response = BytesIO()
         response.write(b'This is POST request. ')
         response.write(b'Received: ')
@@ -55,5 +126,5 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         #self.end_headers()
 
 
-httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('localhost', 8080), SimpleHTTPRequestHandler)
 httpd.serve_forever()
